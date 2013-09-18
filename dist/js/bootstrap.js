eea5a6ad94bea503f7b611af7aaf9806da69a050
Nathan Muir <ndmuir@gmail.com>
2013-09-18 14:53:42 +1000

    Customised variables

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1380,10 +1380,12 @@ if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
   }
 
   Tooltip.prototype.getCalculatedOffset = function (placement, pos, actualWidth, actualHeight) {
-    return placement == 'bottom' ? { top: pos.top + pos.height,   left: pos.left + pos.width / 2 - actualWidth / 2  } :
-           placement == 'top'    ? { top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2  } :
-           placement == 'left'   ? { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth } :
-        /* placement == 'right' */ { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width   }
+    return placement == 'bottom'    ? { top: pos.top + pos.height,   left: pos.left + pos.width / 2 - actualWidth / 2  } :
+           placement == 'top'       ? { top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2  } :
+           placement == 'left'      ? { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth } :
+           placement == 'top-right' ? { top: pos.top - actualHeight, left: pos.left - actualWidth + pos.width } :
+           placement == 'top-left'  ? { top: pos.top - actualHeight, left: pos.left } :
+        /* placement == 'right' */    { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width   }
   }
 
   Tooltip.prototype.getTitle = function () {
